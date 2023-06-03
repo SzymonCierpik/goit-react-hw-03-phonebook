d285dd8769d1d4b33f04dbcd216c9b300e1a5670
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-06-03 19:22:16 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/SzymonCierpik/goit-react-hw-04-phonebook.git",
+  base: "/goit-react-hw-04-phonebook/",
 });
