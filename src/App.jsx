390723e644e0721a4351dd99b2c252a0e12da82a
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-06-05 10:24:17 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import ContactList from "./components/ContactList";
+import ContactForm from "./components/ContactForm";
+import Filter from "./components/Filter";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [contacts, setContacts] = useState([
+    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+  ]);
+
+  const [filter, setFilter] = useState("");
+
+  const addContact = (newContact) => {
+    setContacts([...contacts, newContact]);
+  };
+
+  const deleteContact = (id) => {
+    setContacts(contacts.filter((contact) => contact.id !== id));
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Książka Telefoniczna</h1>
+      <ContactForm addContact={addContact} contacts={contacts} />
+      <h2>Kontakty</h2>
+      <Filter filter={filter} setFilter={setFilter} />
+      <ContactList
+        contacts={contacts.filter((contact) =>
+          contact.name.toLowerCase().includes(filter.toLowerCase())
+        )}
+        deleteContact={deleteContact}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
